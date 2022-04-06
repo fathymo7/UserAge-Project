b46b4daf2320ae8a238be660e9474986eb910e58
fathymo7 <FATHYmohsen47@gmail.com>
2022-04-06 06:05:46 +0200

    Update AddUser.js

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,21 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 import Button from "../UI/Button";
 
 const AddUser = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
   const addUserHandler = (event) => {
     event.preventDefault();
+    console.log(enteredName, enteredAge);
+  };
+
+  const addNameChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const addAgeChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
   };
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">UserName</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" onChange={addNameChangeHandler} />
 
         <label htmlFor="age">Age</label>
-        <input id="age" type="number" />
+        <input id="age" type="number" onChange={addAgeChangeHandler} />
 
         <Button type="submit">Add User</Button>
       </form>
